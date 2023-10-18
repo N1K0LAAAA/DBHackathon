056b8fa0d4704d8dfef30692294f4c72b66e7470
N1K0LAAAA <nikolapraktikum@gmail.com>
2023-10-18 18:36:54 +0200

    getting all Data

--- backend/api.js
+++ backend/api.js
@@ -2,11 +2,17 @@ const express = require("express");
 const betterSQLite3 = require("better-sqlite3");
 
 const api = express.Router();
-const db = betterSQLite3(__dirname + "/hackathonDB.db", { "fileMustExist": true, "verbose": console.log });
+const db = betterSQLite3(__dirname + "/databaseHAc.db", { "fileMustExist": true, "verbose": console.log });
 
 const dbQueries = {
   findUserByEmail: db.prepare("SELECT * FROM Users WHERE email = ?"),
-
+  findUserById: db.prepare("SELECT * FROM Users WHERE user_id = ?"),
+  getUserData: db.prepare("SELECT * FROM Account WHERE user_id = ?"),
+  getTransactionHistory: db.prepare(`
+    SELECT * FROM Transactions
+    WHERE from_Account IN (SELECT account_id FROM Account WHERE user_id = ?)
+       OR to_Account IN (SELECT account_id FROM Account WHERE user_id = ?);
+  `),
 };
 
 api.all("/login", (req, res) => {
@@ -26,6 +32,32 @@ api.all("/login", (req, res) => {
     } else {
       // Passwords do not match
       res.status(401).json({ message: "Incorrect password" });
+    }
+  }
+});
+
+api.all("/user-data/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  // Check if the user exists in the database
+  const user = dbQueries.findUserById.get(userId);
+
+  if (!user) {
+    // User not found
+    res.status(404).json({ message: "User not found" });
+  } else {
+    // Retrieve user account data from the database
+    const userData = dbQueries.getUserData.all(user.user_id); // Use .all() to retrieve multiple rows
+
+    // Retrieve transaction history for the user
+    const transactionHistory = dbQueries.getTransactionHistory.all(user.user_id, user.user_id);
+
+    if (userData.length > 0) {
+      // Send user data and transaction history as a response
+      res.json({ user: userData, transactions: transactionHistory });
+    } else {
+      // User data not found
+      res.status(404).json({ message: "User data not found" });
     }
   }
 });
