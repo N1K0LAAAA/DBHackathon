b4cae813302bc8da981197a50c5a3ee83bef507f
Yoshibt123 <142670589+Yoshibt123@users.noreply.github.com>
2023-10-19 15:18:20 +0200

    fertig?

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -1,24 +1,25 @@
-var x =0 ;
+
 let a = ["Lebensmittel", "Einkäufe", "Transport", "Versicherungen", "Freizeit", "Urlaub"];
 let b = ["Chips", "PC", "Ticket", "Krankenkasse", "Kiten", "Italien-Reise"]
+import { CountUp } from './countUp.js';
 
 function append(){
-  for (let i = 0; i < 12; i++) {
-    
-    
-  
-    x = x+1
-    f = Math.floor(Math.random()*6);
-    g = a[f];
-    z = b[f];
+  let x = 0;
+  fetch("http://alessio.ddnss.de/api/user-data/1").then(response => {
+    response.json().then(data =>{
+    for(let i=0;i<data.transactions.length;i++){
+
+    let f = Math.floor(Math.random()*6);
+    let g = a[f];
+    let z = b[f];
     var jsonObject = {
       className: "list-group-item",
-      eId:`${x}list-group-item`,
-      p:`${z}`,
-      pId:`${x}name`,
-      dId:`${x}box-cat`,
-      cId:`${x}categories`,
-      iH:`${g}`,
+      eId:`${i}list-group-item`,
+      pId:`${i}name`,
+      dId:`${i}box-cat`,
+      
+
+
       pC:"name",
       dC:"box-cat",
       cc:"categories",
@@ -28,43 +29,65 @@ function append(){
     /// for append div and get div object back from js
     var elementProto = JSON.parse(jsonString);
     
-    var element = document.createElement("li");
-    element.className = elementProto.className;
-    element.id = elementProto.eId;
-    
-    // append to container (in your case its page 1 or 2)
-    
-    document.getElementById("list-group").append(element);
 
-    var element = document.createElement("p");
-    element.id = elementProto.pId;
-    element.innerHTML = elementProto.p;
-    element.className = elementProto.pC
-    // append to container (in your case its page 1 or 2)
     
-    document.getElementById(`${x}list-group-item`).append(element);
-    
-
-    var element = document.createElement("div");
-    element.id = elementProto.dId;
-    element.className = elementProto.dC;
-    // append to container (in your case its page 1 or 2)
-    
-    document.getElementById(`${x}list-group-item`).append(element);
+      var element = document.createElement("li");
+      element.className = elementProto.className;
+      element.id = elementProto.eId;
+      
+      // append to container (in your case its page 1 or 2)
+      
+      document.getElementById("list-group").append(element);
+  
+      var element = document.createElement("p");
+      element.id = elementProto.pId;
+      element.innerHTML = z;
+      element.className = elementProto.pC
+      // append to container (in your case its page 1 or 2)
+      
+      document.getElementById(`${i}list-group-item`).append(element);
 
 
-    var element = document.createElement("div");
-    element.id = elementProto.cId;
-    element.className = elementProto.cc;
-    element.innerHTML = elementProto.iH;
-    
-    // append to container (in your case its page 1 or 2)
-    
-    document.getElementById(`${x}box-cat`).append(element);
+      var element = document.createElement("p");
+      element.id = elementProto.pId;
+      if (data.transactions[i].from_Account == 1) {
+        element.innerHTML = '-'+data.transactions[i].amount+'€';
+        element.className = 'negativ';
+        x = x-data.transactions[i].amount;
+      } else {
+        element.innerHTML = '+'+data.transactions[i].amount+'€';
+        element.className = 'positiv';
+        x = x-data.transactions[i].amount;
+      }
+      // append to container (in your case its page 1 or 2)
+      
+      document.getElementById(`${i}list-group-item`).append(element);
+      
+  
+      var element = document.createElement("div");
+      element.id = elementProto.dId;
+      element.className = elementProto.dC;
+      // append to container (in your case its page 1 or 2)
+      
+      document.getElementById(`${i}list-group-item`).append(element);
+  
+  
+      var element = document.createElement("div");
+      element.id = elementProto.cId;
+      element.className = elementProto.cc;
+      element.innerHTML = g;
+      
+      // append to container (in your case its page 1 or 2)
+      
+      document.getElementById(`${i}box-cat`).append(element);
+  
+      
+
+
+
+
 
     }
-    fetch("http://alessio.ddnss.de/api/user-data/1").then(response => {
-    response.json().then(data =>{
     var element = document.createElement("h3");
     element.innerHTML = "Hallo "+data.first_name+ "!";
     element.id = data.user_id;
@@ -72,6 +95,16 @@ function append(){
     // append to container (in your case its page 1 or 2
     document.getElementById("deinName").append(element);
 
-      })  
+    
+    var countUp = new CountUp('lW', data.account_data[0].balance
+    );
+    countUp.start();
+      })
     });
+    
+    
+  
+   
+    
   }
+  append()
