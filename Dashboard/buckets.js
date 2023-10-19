b3b38bf351fe92ea5ae61700b9894438da71de22
Yoshibt123 <142670589+Yoshibt123@users.noreply.github.com>
2023-10-19 16:42:57 +0200

    Request

--- Dashboard/buckets.js
+++ Dashboard/buckets.js
@@ -1,28 +1,29 @@
 import { CountUp } from './countUp.js';
-
+ const userId =1;
 
 function load(){
 
    
    fetch("http://alessio.ddnss.de/api/user-data/1").then(response => {
     response.json().then(data =>{
-   
+        const key = Object.keys(data).find(user => data[user].user_id === '1');
+
    
     
     var element = document.createElement("h3");
-    element.innerHTML = "Hallo "+data.first_name+ "!";
-    element.id = data.user_id;
+    element.innerHTML = "Hallo "+data[key].first_name+ "!";
+    element.id = data[key].user_id;
     
     // append to container (in your case its page 1 or 2
     document.getElementById("deinName").append(element);
 
-    let contentKontoEuro = data.buckets.find(x => x.user_id === 1).food+data.buckets.find(x => x.user_id === 1).transport+data.buckets.find(x => x.user_id === 1).living_expense+data.buckets.find(x => x.user_id === 1).insurance+data.buckets.find(x => x.user_id === 1).entertainment+data.buckets.find(x => x.user_id === 1).urlaub;
-    let Lebensmittel= data.buckets.find(x => x.user_id === 1).food;
-    let Transport = data.buckets.find(x => x.user_id === 1).transport;
-    let Einkäufe = data.buckets.find(x => x.user_id === 1).living_expense;
-    let Versicherungen = data.buckets.find(x => x.user_id === 1).insurance;
-    let Freizeit = data.buckets.find(x => x.user_id === 1).entertainment;
-    let Urlaub = data.buckets.find(x => x.user_id === 1).urlaub;
+    let contentKontoEuro = data.buckets.find(x => x.user_id === userId).food+data.buckets.find(x => x.user_id === 1).transport+data.buckets.find(x => x.user_id === 1).living_expense+data.buckets.find(x => x.user_id === 1).insurance+data.buckets.find(x => x.user_id === 1).entertainment+data.buckets.find(x => x.user_id === 1).urlaub;
+    let Lebensmittel= data.buckets.find(x => x.user_id === userId).food;
+    let Transport = data.buckets.find(x => x.user_id === userId).transport;
+    let Einkäufe = data.buckets.find(x => x.user_id === userId).living_expense;
+    let Versicherungen = data.buckets.find(x => x.user_id === userId).insurance;
+    let Freizeit = data.buckets.find(x => x.user_id === userId).entertainment;
+    let Urlaub = data.buckets.find(x => x.user_id === userId).urlaub;
     
     var countUp = new CountUp('kontoEuro', contentKontoEuro);
     countUp.start();
--- Dashboard/index.js
+++ Dashboard/index.js
@@ -2,12 +2,15 @@
 let a = ["Lebensmittel", "Einkäufe", "Transport", "Versicherungen", "Freizeit", "Urlaub"];
 let b = ["Chips", "PC", "Ticket", "Krankenkasse", "Kiten", "Italien-Reise"]
 import { CountUp } from './countUp.js';
+const userId = 1
 
 function append(){
   let x = 0;
   fetch("http://alessio.ddnss.de/api/user-data/1").then(response => {
     response.json().then(data =>{
-    for(let i=0;i<data.transactions.length;i++){
+      const key = Object.keys(data).find(user => data[user].user_id === '1');
+
+    for(let i=0;i<data[key].transactions.length;i++){
 
     let f = Math.floor(Math.random()*6);
     let g = a[f];
@@ -50,14 +53,14 @@ function append(){
 
       var element = document.createElement("p");
       element.id = elementProto.pId;
-      if (data.transactions[i].from_Account == 1) {
-        element.innerHTML = '-'+data.transactions[i].amount+'€';
+      if (data[key].transactions[i].from_Account == 1) {
+        element.innerHTML = '-'+ data[key].transactions[i].amount+'€';
         element.className = 'negativ';
-        x = x-data.transactions[i].amount;
+        x = x-data[key].transactions[i].amount;
       } else {
-        element.innerHTML = '+'+data.transactions[i].amount+'€';
+        element.innerHTML = '+'+data[key].transactions[i].amount+'€';
         element.className = 'positiv';
-        x = x-data.transactions[i].amount;
+        x = x-data[key].transactions[i].amount;
       }
       // append to container (in your case its page 1 or 2)
       
@@ -89,7 +92,7 @@ function append(){
 
     }
     var element = document.createElement("h3");
-    element.innerHTML = "Hallo "+data.first_name+ "!";
+    element.innerHTML = "Hallo "+data.find(x => x.user_id === userId).first_name+ "!";
     element.id = data.user_id;
     
     // append to container (in your case its page 1 or 2
