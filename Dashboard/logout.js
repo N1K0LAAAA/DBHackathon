1788761967ef7532ad15d3f0625bb526c82cc8f2
N1K0LAAAA <nikolapraktikum@gmail.com>
2023-10-20 10:54:53 +0200

    alles

--- Dashboard/logout.js
+++ Dashboard/logout.js
@@ -1,3 +1,10 @@
-function logout(){
-    alert('Du wirst ausgelogt')
+function logout() {
+    // Clear the "user_id" cookie
+    document.cookie = "user_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+    // Alert the user that they have been logged out
+    alert('Du wurdest ausgelogt');
+    
+    // Redirect the user to the login page or another appropriate page
+    window.location.href = "index.html"; // Change the URL as needed
 }
